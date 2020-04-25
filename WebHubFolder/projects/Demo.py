b3b56a52d2b621dc72daa707e48d04141cfb95d7
Lualts-code <58912038+Lualts-code@users.noreply.github.com>
2020-04-25 11:29:43 +0200

    Delete Demo.py

--- WebHubFolder/projects/Demo.py
+++
@@ -1,9 +0,0 @@
-from WebHubFolder import WebHubMaker
-
-WebHubMaker.window(title="Demo", height="100", width="225")
-WebHubMaker.title(text="This is a demo!", font="helvetica")
-WebHubMaker.text(text="This size is 15.", font="helvetica", size="15", fg="black", bg="white")
-WebHubMaker.link(text="You also can use links!", link="https://google.com", size="12", font="helvetica")
-WebHubMaker.dropdown(items=["Or", "dropdown", "menu's."], default="Or")
-
-WebHubMaker.start()
