0c819bc98385bf173227de825006e8c54d127bd3
Lualts-code <58912038+Lualts-code@users.noreply.github.com>
2020-04-26 09:25:49 +0200

    Add files via upload

--- WebHubFolder/Projects/Demo.py
+++ WebHubFolder/Projects/Demo.py
@@ -1,9 +1,9 @@
 from WebHubFolder import WebHubMaker
 
-WebHubMaker.window(title="Demo", height="100", width="225")
-WebHubMaker.title(text="This is a demo!", font="helvetica")
-WebHubMaker.text(text="This size is 15.", font="helvetica", size="15", fg="black", bg="white")
-WebHubMaker.link(text="You also can use links!", link="https://google.com", size="12", font="helvetica")
-WebHubMaker.dropdown(items=["Or", "dropdown", "menu's."], default="Or")
+WebHubMaker.window(title="Demo", height="100", width="225", bg="red")
+WebHubMaker.title(text="This is a demo!", font="helvetica", fg="yellow", bg="red")
+WebHubMaker.text(text="This size is 15.", font="helvetica", size="15", fg="black", bg="red")
+WebHubMaker.link(text="You also can use links!", link="https://google.com", size="12", font="helvetica", bg="red")
+WebHubMaker.dropdown(items=["Or", "dropdown", "menu's."], default="Or", fg="yellow", bg="red")
 
 WebHubMaker.start()
